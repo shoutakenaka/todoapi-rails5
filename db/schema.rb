7166e54de413452c61f9740367169f7ebe950702
Shou Takenaka <sasaminn@gmail.com>
2019-05-24 15:41:15 +0000

    Define sessions table schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_153325) do
+ActiveRecord::Schema.define(version: 2019_05_24_153827) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
