6de84db6611203777dc37c7fcf36f94a0a871d9f
Shou Takenaka <sasaminn@gmail.com>
2019-05-24 15:37:47 +0000

    Add relation between user and task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true
   validates :priority, inclusion: { in: %w(low medium high) }, allow_blank: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  
+  has_many :tasks
+
+  validates :username, presence: true
+  validates :password, presence: true
 end
