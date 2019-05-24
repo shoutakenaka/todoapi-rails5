57aa6e54a82f5f99e180595ffb4beaed9a61ee49
Shou Takenaka <sasaminn@gmail.com>
2019-05-24 15:37:37 +0000

    Add user_id to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_153124) do
+ActiveRecord::Schema.define(version: 2019_05_24_153325) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_05_24_153124) do
     t.string "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
